5b2ba924da9b473e25675b9e0736ce0dfc40265e
刘少甫 <liushaofu>
2018-07-18 10:40:54 +0800

    ad

---
+++ src/main/java/com/wen/entity/Girl.java
@@ -0,0 +1,53 @@
+package com.wen.entity;
+
+public class Girl {
+    private Integer id;
+
+    private Integer age;
+
+    private String cupSize;
+
+    private Double money;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getCupSize() {
+        return cupSize;
+    }
+
+    public void setCupSize(String cupSize) {
+        this.cupSize = cupSize == null ? null : cupSize.trim();
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        return "Girl{" +
+                "id=" + id +
+                ", age=" + age +
+                ", cupSize='" + cupSize + '\'' +
+                ", money=" + money +
+                '}';
+    }
+}
